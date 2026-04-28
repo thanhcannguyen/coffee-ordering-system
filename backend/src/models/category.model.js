c9813e46e8ffd62f57843e877e090765a2a6c4a5
Nguyen Thanh Can <nguyenthanhcan3004@gmail.com>
2026-04-28 12:10:16 +0700

    feat(category): add category model

---
+++ backend/src/models/category.model.js
@@ -0,0 +1,31 @@
+
+import mongoose from 'mongoose'
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        minLength: 2,
+        maxLength: 50
+    },
+    description: {
+        type: String,
+        trim: true,
+        default: '',
+        maxLength: 500
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+        index: true
+    },
+}, {
+    timestamps: true
+})
+
+const Category = mongoose.model("Category", categorySchema)
+
+export default Category
